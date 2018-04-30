e9003a8964f67f123698c7c286d36dfc87305efd
Marco Aguilera <maaguilera@csumb.edu>
2018-04-30 04:18:51 +0000

    Fourth Commit

--- dbConnection.php
+++ dbConnection.php
@@ -1,8 +1,8 @@
 <?php   
     function connectDB($dbName){
-        $host = "localhost";
-        $username = "root";
-        $password = "";
+        $host = "us-cdbr-iron-east-05.cleardb.net";
+        $username = "b61f62434be134";
+        $password = "f86a6a18";
         $dbname = "lab8"; 
         $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
         $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
