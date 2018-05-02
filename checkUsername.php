3ee3383d992f595549201d7a61cff4525b17c495
Marco Aguilera <maaguilera@csumb.edu>
2018-05-02 22:15:02 +0000

    Testing some DB connection changes

--- checkUsername.php
+++ checkUsername.php
@@ -1,15 +1,14 @@
 <?php   
-    include './dbConnection.php';
-    
-    $conn = connectDB("heroku_03f28d949defc53");
-    
-    $username = $_GET['username'];
-    $sql = "SELECT * FROM lab8_user WHERE username = :username ";
-    
-    $stmt = $conn->prepare($sql);
-    $stmt->execute(array(":username"=> $username));
-    $record = $stmt->fetch(PDO::FETCH_ASSOC);
-    
-    // print_r($record);
-    echo json_encode($record);
-?>
+-    include 'dbConnection.php';
+     
+     $conn = connectDB();
+     
+     $username = $_GET['username'];
+     $sql = "SELECT *FROM lab8_user WHERE username = '$username' ";
+     
+     $stmt = $conn->prepare($sql);
+     $stmt->execute(array(":username"=> $username));
+     $record = $stmt->fetch(PDO::FETCH_ASSOC);
+     
+     echo json_encode($record);
+ ?>
--- dbConnection.php
+++ dbConnection.php
@@ -1,19 +1,16 @@
-<?php   
-    function connectDB($dbName){
-        $host = "us-cdbr-iron-east-05.cleardb.net";
-        $username = "b61f62434be134";
-        $password = "f86a6a18";
-        $dbname = $dbName; 
-        $charset = 'utf8mb4';
-    
-        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-        $opt = [
-            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES   => false,
-        ];
-        $pdo = new PDO($dsn, $user, $pass, $opt);
-        return $pdo; 
-        }
-    
-?>
+ <?php   
+     function connectDB(){
+
+         $username = "b61f62434be134";
+         $password = "f86a6a18";
+         $host = "us-cdbr-iron-east-05.cleardb.net";
+         $username = "b61f62434be134";
+         $password = "f86a6a18";
+         $dbname = "heroku_03f28d949defc53"; 
+         $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+         $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ 
+        return $dbConn; 
+     }
+     
+ ?>
